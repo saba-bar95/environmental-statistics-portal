05cb424a448037c8e68be5bc147409b9070be998
saba-bar95 <s.barbakadze95@gmail.com>
2026-02-12 14:35:20 +0400

    change hardcodded numbers on waste page

--- src/assets/components/Homepage/Slider/Slides/Texts/Waste/text.js
+++ src/assets/components/Homepage/Slider/Slides/Texts/Waste/text.js
@@ -1,7 +1,7 @@
 const text = {
   ge: {
     header: "მუნიციპალური ნარჩენები, სულ",
-    number1: "1116.6",
+    number1: "1184",
     unit1: "ათასი ტონა ნარჩენი - 2023",
     number2: "7%",
     para: "წინა წელთან შედარებით",
@@ -9,7 +9,7 @@ const text = {
 
   en: {
     header: "Municipal waste, total",
-    number1: "1116.6",
+    number1: "1184",
     unit1: "Thousands of tons of waste - 2023",
     number2: "7%",
     para: "Compared to the previous year",
